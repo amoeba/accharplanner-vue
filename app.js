85f2d926de96693d695be8a8f3a3c896ff683ee2
Bryce Mecum <mecum@nceas.ucsb.edu>
2016-06-11 12:17:52 -0800

    Remove a done TODO item

--- app.js
+++ app.js
@@ -139,8 +139,6 @@ var untrained_state = {
 };
 
 // TODO: Add consts for unspeccable (eg armor tink)
-// TODO: Add consts for non-untrainable (eg healing)
-var credits_by_level = { 
 var credits_by_level = {
   1: 0, 
 	2: 1,
